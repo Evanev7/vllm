15b3e44bf59e8e8eaac414c045db145e103a1f04
Evan <evanev7@gmail.com>
2026-04-24 17:47:51 +0100

    actually pull CUDA_HOME

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ class cmake_build_ext(build_ext):
                 num_jobs = os.cpu_count()
 
         nvcc_threads = None
-        if _is_cuda() and CUDA_HOME is not None:
+        if _is_cuda() and envs.CUDA_HOME is not None:
             try:
                 nvcc_version = get_selected_cuda_version()
                 if nvcc_version >= Version("11.2"):
@@ -239,8 +239,8 @@ class cmake_build_ext(build_ext):
             # Default build tool to whatever cmake picks.
             build_tool = []
         # Make sure we use the nvcc from CUDA_HOME
-        if _is_cuda() and CUDA_HOME is not None:
-            cmake_args += [f"-DCMAKE_CUDA_COMPILER={CUDA_HOME}/bin/nvcc"]
+        if _is_cuda() and envs.CUDA_HOME is not None:
+            cmake_args += [f"-DCMAKE_CUDA_COMPILER={envs.CUDA_HOME}/bin/nvcc"]
         elif _is_hip() and ROCM_HOME is not None:
             cmake_args += [f"-DROCM_PATH={ROCM_HOME}"]
 
@@ -785,9 +785,9 @@ def get_nvcc_cuda_version() -> Version:
 
     Adapted from https://github.com/NVIDIA/apex/blob/8b7a1ff183741dd8f9b87e7bafd04cfde99cea28/setup.py
     """
-    assert CUDA_HOME is not None, "CUDA_HOME is not set"
+    assert envs.CUDA_HOME is not None, "CUDA_HOME is not set"
     nvcc_output = subprocess.check_output(
-        [CUDA_HOME + "/bin/nvcc", "-V"], universal_newlines=True
+        [envs.CUDA_HOME + "/bin/nvcc", "-V"], universal_newlines=True
     )
     output = nvcc_output.split()
     release_idx = output.index("release") + 1
@@ -810,7 +810,7 @@ if _is_hip():
 if _is_cuda():
     ext_modules.append(CMakeExtension(name="vllm.vllm_flash_attn._vllm_fa2_C"))
     if envs.VLLM_USE_PRECOMPILED or (
-        CUDA_HOME and get_selected_cuda_version() >= Version("12.3")
+        envs.CUDA_HOME and get_selected_cuda_version() >= Version("12.3")
     ):
         # FA3 requires CUDA 12.3 or later
         ext_modules.append(CMakeExtension(name="vllm.vllm_flash_attn._vllm_fa3_C"))
@@ -820,7 +820,7 @@ if _is_cuda():
         CMakeExtension(name="vllm.vllm_flash_attn._vllm_fa4_cutedsl_C", optional=True)
     )
     if envs.VLLM_USE_PRECOMPILED or (
-        CUDA_HOME and get_selected_cuda_version() >= Version("12.9")
+        envs.CUDA_HOME and get_selected_cuda_version() >= Version("12.9")
     ):
         # FlashMLA requires CUDA 12.9 or later
         # Optional since this doesn't get built (produce an .so file) when
